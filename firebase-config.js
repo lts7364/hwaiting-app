f22aac82cdde094b1c20eccecd01fd9aaf16669e
lts7364 <lts7364@gmail.com>
2026-06-04 15:17:27 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,17 @@
-// 화이팅 앱 최신 APK 파일명
-// GitHub 저장소 files/ 폴더 안에 이 파일명이 실제로 있어야 합니다.
-const APK_FILE = "hwaiting-v1-0-103-code104-k7p4x9.apk";
+export const firebaseConfig = {
+  apiKey: "AIzaSyDFGejarmyxZLLAvHzJDtHcZCafqxKZrl0",
+  authDomain: "hwaiting-app.firebaseapp.com",
+  projectId: "hwaiting-app",
+  storageBucket: "hwaiting-app.firebasestorage.app",
+  messagingSenderId: "836771722326",
+  appId: "1:836771722326:web:9035567474ab2785adf7f7"
+};
+
+export const ADMIN_EMAIL = "lts7364@gmail.com";
+
+export const APP_VERSION = {
+  versionName: "1.0.103-native",
+  versionCode: 104
+};
+
+export const APK_FILE = "files/hwaiting-v1-0-103-code104-k7p4x9.apk";
