9ef79e3602ccf8f9435655d11094aa5124fd3191
lts7364 <lts7364@gmail.com>
2026-06-14 17:46:05 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -10,8 +10,8 @@ export const firebaseConfig = {
 export const ADMIN_EMAIL = "lts7364@gmail.com";
 
 export const APP_VERSION = {
-  versionName: "1.0.127-native",
-  versionCode: 128
+  versionName: "1.0.128-native",
+  versionCode: 129
 };
 
-export const APK_FILE = "files/hwaiting-v1-0-127-code128-k7p4x9.apk";
+export const APK_FILE = "files/hwaiting-v1-0-128-code129-k7p4x9.apk";
